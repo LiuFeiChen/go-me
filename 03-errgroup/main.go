c0f9a3b74278afbb61e8a4a7b09d0e2852f30755
LiuFei Chen <chenliufei163@163.com>
2021-06-14 02:00:49 +0800

    add comment

--- 03-errgroup/main.go
+++ 03-errgroup/main.go
@@ -1,39 +1,50 @@
 package main
 
-
 import (
 	"context"
 	"errors"
 	"fmt"
 	"golang.org/x/sync/errgroup"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
+// api handler test
+func apiHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintln(w, "hello world")
+}
+
+
 func main(){
 
-	fContext := context.Background()
-	ctx_, cancel := context.WithCancel(fContext)
+	ctx_, cancel := context.WithCancel(context.Background())
 	errG, ctx := errgroup.WithContext(ctx_)
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "hello world")
-	})
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/test", apiHandler)
+
 	srv := http.Server{
 		Addr:              ":8081",
-		Handler:           handler,
+		Handler:           mux,
 	}
 
+	//当收到退出信号时，cancel()，此时服务shutdown
 	errG.Go(func() error {
 		<-ctx.Done()
 		return srv.Shutdown(context.Background())
 	})
+	//启动服务
 	errG.Go(func() error {
 		return srv.ListenAndServe()
 	})
+
+	//注册信号处理
 	signalC := make(chan os.Signal, 1)
-	signal.Notify(signalC, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	signal.Notify(signalC, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT)
+
 	errG.Go(func() error {
 		for {
 			select {
@@ -44,7 +55,8 @@ func main(){
 			}
 		}
 	})
-	if err := errG.Wait(); err != nil && !errors.Is(err, context.Canceled){
-		fmt.Printf("server stop: %+V\n", err)
+
+	if err := errG.Wait(); err != nil &&  !errors.Is(err, context.Canceled)  {
+		log.Fatal(err)
 	}
 }
